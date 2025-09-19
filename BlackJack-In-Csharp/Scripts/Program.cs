dec3d9273c7e1d24d9587101072a99acbe20af08
TucanoiDEV <rodrigohenrique987@gmail.com>
2025-09-19 12:10:58 -0300

    First Commit

---
+++ BlackJack-In-Csharp/Scripts/Program.cs
@@ -0,0 +1,23 @@
+﻿using Gameplay;
+
+public class BlackJackMain
+{
+    static void Main()
+    {
+        //Var for the player name
+        string name;
+
+        Console.WriteLine("WELCOME TO MY NEW BLACKJACK GAME!!!");
+        Console.ReadKey();
+        Console.WriteLine("Press any key when you are ready!");
+        Clear();
+
+
+    }
+
+    public static void Clear()
+    {
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
