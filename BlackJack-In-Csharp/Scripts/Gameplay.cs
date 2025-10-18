d8b7f43d38db9a54c39e6df1364b1c5ce387e2cc
TucanoiDEV <rodrigohenrique987@gmail.com>
2025-10-18 17:38:26 -0300

    desenvolvimento do método gameplay

--- BlackJack-In-Csharp/Scripts/Gameplay.cs
+++ BlackJack-In-Csharp/Scripts/Gameplay.cs
@@ -12,20 +12,43 @@ namespace Gameplay
             //Alocação de variável name de CreateName para o arquivo de Gameplay
             string playerName = CreateNameClass.name;
 
-            do
+            if(chips > 0)
             {
-                Console.WriteLine($"You have {chips} chips");
-                Console.Write($"{playerName}, Enter the amount to bet: ");
-                bet = Console.ReadLine();
-
-                if (!int.TryParse(input, out bet) || bet <= 0 || bet > chips)
+            betMoment:
+                Console.Clear();
+                Console.WriteLine($"{playerName} you have {chips} chips.\nHow many chips do you want to bet?");
+                if (int.TryParse(Console.ReadLine(), out bet))
                 {
-                    Console.WriteLine("Invalid bet, please try again");
-                    Console.WriteLine("[Press any key to continue]");
-                    Console.ReadKey();
-                    bet = -1;
+                    if(bet > chips)
+                    {
+                        Console.WriteLine("You cannot bet more chips than you have.");
+                        Console.WriteLine("Press any key to try again");
+                        Console.ReadKey();
+                    }
+                    if(bet == 0)
+                    {
+                        Console.WriteLine("you need to bet at least 1 chip");
+                        Console.WriteLine("Press any key to try again");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+
+                    }
                 }
-            } while(bet <= 0 || bet > chips);
+                else
+                {
+                    Console.WriteLine("Invalid bet amount, press eny key to try again");
+                    Console.ReadKey();
+                    goto betMoment;
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("You don´t have any tips ;(");
+            }
         }
     }
 }
